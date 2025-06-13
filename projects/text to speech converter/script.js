f68fd486a6221cb448fcdafa500a14117e5c78d5
mariahashmii <maria.hashmi292006@gmail.com>
2025-06-13 23:49:04 +0530

    Update script.js

--- projects/text to speech converter/script.js
+++ projects/text to speech converter/script.js
@@ -1,15 +1,27 @@
-let speech=new SpeechSynthesisUtterance();
-let voices=[];
-let voiceSel=document.querySelector("select");
-window.speechSynthesis.onvoiceschanged=()=>{
-    voices=window.speechSynthesis.getVoices();
-    speech.voice=voices[0];
-    voices.forEach((voice,i)=>(voiceSel.options[i]=new Option(voice.name,i)));
-};
-voiceSel.addEventListener("change",()=>{
-    speech.voice=voices[voiceSel.value];
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSel = document.querySelector("select");
+function loadVoices() {
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length > 0) {
+        voiceSel.innerHTML = ""; // clear previous
+        voices.forEach((voice, i) => {
+            let option = new Option(voice.name + ' (' + voice.lang + ')', i);
+            voiceSel.add(option);
+        });
+        speech.voice = voices[0];
+    }
+}
+window.speechSynthesis.onvoiceschanged = loadVoices;
+setTimeout(() => {
+    if (!voices.length) loadVoices();
+}, 500);
+
+voiceSel.addEventListener("change", () => {
+    speech.voice = voices[voiceSel.value];
 });
-document.querySelector("button").addEventListener("click",()=>{
-    speech.text=document.querySelector("textarea").value;
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
-})
+});
